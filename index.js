6a9eadea827b869160f89ba712237db852b1c912
Sabely Sabely <enriquezsabely@gmail.com>
2024-09-01 17:43:29 -0400

    added the frist function

--- index.js
+++ index.js
@@ -30,3 +30,9 @@ const questions = [
     },
    
 ];
+
+function writeFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.log(err) : console.log ('SVG Logo Completed')
+    );
+}
