b85080e90f082a1e91ac54e0942ad3203b5782b0
Sabely Sabely <enriquezsabely@gmail.com>
2024-09-01 17:38:12 -0400

    added the questions

---
+++ index.js
@@ -0,0 +1,32 @@
+const inquirer = require ('inquirer');
+const fs =require = require('fs');
+const { Circle, Square} = require('');
+
+const questions = [
+    {
+        type:'input',
+        name:'Title',
+        message:'Enter 3 characters for your title:',
+
+    },
+    {
+        type:'input',
+        name:'TitleColor',
+        message:'Enter a color for your title',
+
+    },
+    {
+        type:'list',
+        name:'Shape',
+        message:'choose a shape:',
+        choices:['Circle','Square'],
+
+    },
+    {
+        type:'input',
+        name:'ShapeColor',
+        message:'Enter a color for the shape',
+
+    },
+   
+];
