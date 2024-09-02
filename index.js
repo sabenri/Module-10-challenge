8807ce4723127502a494ef3746b76aaffb04556a
Sabely Sabely <enriquezsabely@gmail.com>
2024-09-01 20:26:24 -0400

    finish off the index.js

--- index.js
+++ index.js
@@ -36,3 +36,61 @@ function writeFile(fileName, data) {
         err ? console.log(err) : console.log ('SVG Logo Completed')
     );
 }
+class svgProperties{
+    constructor(){
+        this.Text ='';
+        this.Shape ='';
+
+    }
+render() {
+    return `
+    <svg width="500" height="500" xmlns="http://www.w3.org/2000/svg">
+        ${this.Shape}
+        ${this.Text}
+    </svg>
+    `;
+}
+
+setText(text, color) {
+    this.Text =`
+    <text x="125" y="125" font-size="25" text-anchor="middle" fill="${color}">${text}</text>`;
+}
+setShape(Shape) {
+    this.Shape = Shape.render();
+}
+}
+
+async function run() { 
+    const response = await inquirer.prompt (questions);
+    let {title, TitleColor, Shape, ShapeColor} = response;
+
+    if (TitleColor.lenght >3) {
+        console.log ('Please enter no more that 3 characters');
+        return;
+    }
+    
+    console.log('${Title} is the title for your logo');
+    console.log('${TitleColor} is the color of the text');
+    console.log('${Shape} is the shape for your logo');
+    console,log('${ShapeColor} is the color for your logo');
+
+    let shapeObj; 
+    switch (shape) {
+        case 'shape':
+            shapeObj = new Circle();
+            break;
+            case 'Square':
+                shapeObj = new Square
+                break;
+    }
+
+    shapeObj.ShapeColor(ShapeColor);
+    const svgFile = new svgProperties();
+    svgFile.setText(Title, TitleColor);
+    svgFile.setShape(shapeObj);
+    const svg = svgFile.render();
+
+    writeToFile('logo.svg', svg)   
+}
+
+run();
